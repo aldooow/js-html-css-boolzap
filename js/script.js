cc3fcea651872aa5851b3ea30fbe246d2211a951
Aldo Cahuana <aldooow@icloud.com>
2020-06-16 21:30:43 +0200

    Add Keydown & Keyup.
    Work in progress!!!

--- js/script.js
+++ js/script.js
@@ -5,16 +5,36 @@ $(document).ready(
     /*********************************/
 
     // Click su .... , per aprire le opzioni
-    $('#js_enter').click(
-      function(){
+    // $('#js_enter').click(
+    //   function(){
+    //
+    //     var message = $('.write-message input').val()
+    //     var template = $('.template').children('.message-me').clone();
+    //     // console.log(template)
+    //     template.text(message)
+    //     $('.wrapper-message').append(template);
+    //   }
+    // );
+
+    $(document).keydown(
+      function(event){
+        if(event.which=='13'){
         var message = $('.write-message input').val()
         var template = $('.template').children('.message-me').clone();
-        console.log(template)
+        // console.log(template)
         template.text(message)
         $('.wrapper-message').append(template);
+        }
+       }
+     );
 
-      }
-    );
+     $(document).keyup(
+       function(event){
+         if(event.which=='13'){
+         $('.write-message input').val('')
+         }
+        }
+      );
 
 
     /*********************************/
