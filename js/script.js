a61360a2aae8235c22ce365c1892133764d9b8b8
Aldo Cahuana <aldooow@icloud.com>
2020-06-16 23:31:35 +0200

    Add  auto reply.
    Work in progress!!!

--- js/script.js
+++ js/script.js
@@ -4,6 +4,27 @@ $(document).ready(
   function(){
     /*********************************/
 
+    /* Risposte Automatiche */
+    var messageFriends = [
+      'Tutto Ok, dai!!',
+      'Non sapevo nulla',
+      'grazie',
+      'non lo sapevo',
+      'Sono contento',
+      'Raccontami qualcosa.',
+      'Vieni a casa!!',
+      'Ci vediamo domani',
+      'Sicuro???'
+    ];
+
+    //  FUNCTION: creaNumeroRandom(): Questa Funzione crea un numero random tenendo in considerazione come range max, la larghezza del array inserito.
+    //   --> array: array da inserire.
+    function creaNumeroRandom(array){
+        var numero = Math.round(Math.random()*(array.length-1));
+        return numero;
+      }
+
+      // Quando faccio "CLICK", si appende il 'INPUT' nel area dei messaggi.
     $('#js_enter').click(
       function(){
 
@@ -13,7 +34,18 @@ $(document).ready(
         template.text(message)
         $('.wrapper-message').append(template);
         // Reset Value
-        $('.write-message input').val('')
+        $('.write-message input').val('');
+
+        /*** Risposta Automatica dopo 2 secondi ***/
+        setTimeout(function(){
+          var numeroRandon = creaNumeroRandom(messageFriends);
+          var template = $('.template').children('.message-he').clone();
+          // console.log(template)
+          template.text(messageFriends[numeroRandon])
+          $('.wrapper-message').append(template);
+          // Reset Value
+        }, 2000);
+        /******/
       }
     );
 
@@ -27,19 +59,21 @@ $(document).ready(
         template.text(message)
         $('.wrapper-message').append(template);
         // Reset Value
-        $('.write-message input').val('')
+        $('.write-message input').val('');
+
+        /*** Risposta Automatica dopo 2 secondi ***/
+        setTimeout(function(){
+          var numeroRandon = creaNumeroRandom(messageFriends);
+          var template = $('.template').children('.message-he').clone();
+          // console.log(template)
+          template.text(messageFriends[numeroRandon])
+          $('.wrapper-message').append(template);
+          // Reset Value
+        }, 2000);
+        /******/
         }
        }
      );
-
-     // // Quando il tasto si alza resset al 'INPUT'.
-     // $(document).keyup(
-     //   function(event){
-     //     if(event.which=='13'){
-     //     $('.write-message input').val('')
-     //     }
-     //    }
-     //  );
 
 
     /*********************************/
