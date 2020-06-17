8a061d7e51e844ed53fe17fa5b2f729d766e3fb8
Aldo Cahuana <aldooow@icloud.com>
2020-06-17 16:13:16 +0200

    Work in progress!!!

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(
     /*********************************/
 
     /* Risposte Automatiche */
-    var messageFriends = [
+    var messagesReceived = [
       'Tutto Ok, dai!!',
       'Non sapevo nulla',
       'grazie',
@@ -17,34 +17,17 @@ $(document).ready(
       'Sicuro???'
     ];
 
-    //  FUNCTION: creaNumeroRandom(): Questa Funzione crea un numero random tenendo in considerazione come range max, la larghezza del array inserito.
-    //   --> array: array da inserire.
-    function creaNumeroRandom(array){
-        var numero = Math.round(Math.random()*(array.length-1));
-        return numero;
-      }
+
 
       // Quando faccio "CLICK", si appende il 'INPUT' nel area dei messaggi.
     $('#js_enter').click(
       function(){
 
-        var message = $('.write-message input').val()
-        var template = $('.template').children('.message-me').clone();
-        // console.log(template)
-        template.text(message)
-        $('.wrapper-message').append(template);
-        // Reset Value
-        $('.write-message input').val('');
+        sendMessage();
+        /* Vissualizare ultimo messaggio */
+        $('.container-message').scrollTop($('.container-message').height());
 
-        /*** Risposta Automatica dopo 2 secondi ***/
-        setTimeout(function(){
-          var numeroRandon = creaNumeroRandom(messageFriends);
-          var template = $('.template').children('.message-he').clone();
-          // console.log(template)
-          template.text(messageFriends[numeroRandon])
-          $('.wrapper-message').append(template);
-          // Reset Value
-        }, 1000);
+
         /******/
       }
     );
@@ -53,27 +36,69 @@ $(document).ready(
     $('.write-message input').keydown(
       function(event){
         if(event.which=='13'){
-        var message = $('.write-message input').val()
-        var template = $('.template').children('.message-me').clone();
-        // console.log(template)
-        template.text(message)
-        $('.wrapper-message').append(template);
-        // Reset Value
-        $('.write-message input').val('');
 
-        /*** Risposta Automatica dopo 2 secondi ***/
-        setTimeout(function(){
-          var numeroRandon = creaNumeroRandom(messageFriends);
-          var template = $('.template').children('.message-he').clone();
-          // console.log(template)
-          template.text(messageFriends[numeroRandon])
-          $('.wrapper-message').append(template);
-          // Reset Value
-        }, 1000);
+        sendMessage();
+        /* Vissualizare ultimo messaggio */
+        $('.container-message').scrollTop($('.container-message').height());
+
+
         /******/
         }
        }
      );
+
+
+
+     /*FUNZIONI*/
+
+  // FUNCTION: creaNumeroRandom(): Questa Funzione crea un numero random tenendo in considerazione come range max, la larghezza del array inserito.
+     //   --> array: array da inserire.
+     function creaNumeroRandom(array){
+         var numero = Math.round(Math.random()*(array.length-1));
+         return numero;
+       }
+   //  FUNCTION: addZeroToNumber(): Questa funzione aggiunge uno zero da vanti a un numero menore di 10.
+     function addZeroToNumber(number){
+       if(number < 10) {
+         return '0' + number;
+       }
+       return number;
+     }
+
+
+   //  FUNCTION: sendMessage(): Questa funzione contiene il messaggio da inviare, e l'ora del messagio. C'e anche  una risposta automatica.
+     function sendMessage(){
+       var message = $('.write-message input').val()
+       var template = $('.template').find('.message-right').clone();
+       // console.log(template)
+
+       // Inserire Ora.
+       var date = new Date();
+       var currentHours = date.getHours();
+       var currentMinutes = date.getMinutes();
+       var currentTime = addZeroToNumber(currentHours) + ':' + addZeroToNumber(currentMinutes);
+       template.find('.message-hour').text(currentTime);
+
+       template.children('.box-right').html(message + '<span class="message-hour">' + currentTime + '</span>');
+       $('.wrapper-message').append(template);
+       // Reset Value
+       $('.write-message input').val('');
+
+       /*** Risposta Automatica dopo 1 secondi ***/
+       setTimeout(function(){
+         var numeroRandon = creaNumeroRandom(messagesReceived);
+         var template = $('.template').find('.message-left').clone();
+         // console.log(template)
+
+         template.children('.box-left').html(messagesReceived[numeroRandon] + '<span class="message-hour">' + currentTime + '</span>');
+         $('.wrapper-message').append(template);
+         // Reset Value
+       }, 1000);
+     }
+
+
+
+     /*END FUNZIONI*/
 
 
     /*********************************/
