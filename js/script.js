516c27b219d7ee0eff98e6f696a26c27d0bcad68
Aldo Cahuana <aldooow@icloud.com>
2020-06-18 21:32:43 +0200

    Work in progress!!

--- js/script.js
+++ js/script.js
@@ -1,19 +1,41 @@
 $(document).ready(
   function(){
 
-activeSettingsMessages()
+    $('.box-contacts .contact').click(
+          function() {
+            // Seleziono dati dell'utente su quale clicco
+              //--> Avatar del contatto.
+            var contactAvatar = $(this).find('img').attr('src');
+              //--> Nome del contatto.
+            var contactName = $(this).find('h4').text();
+              //--> Vissualizazione orario contatto.
+            var contactLastTimeSeen = $(this).find('.time-contact p').text();
 
-$('.js_delete-message').click(
-  function(){
-  $(this).parents('.js_msg').addClass('hidden')
-}
-);
+            // Sostituisco dati del "Header Contact Bar".
+              //--> Sostituisco Avatar del contatto.
+            var currentContactAvatar = $('.header-contact-bar').find('img').attr('src', contactAvatar);
+              //--> Sostituisco Nome del contatto.
+            var currentContactName = $('.header-contact-bar').find('h4').text(contactName);
+              //--> Sostituisco Vissualizazione orario contatto.
+            var currentContactLastTimeSeen = $('.header-contact-bar').find('.ultimo-accesso p').text('ulimo accesso oggi alle ' + contactLastTimeSeen);
+        });
+
+  activeSettingsMessages();
+
+  $('.box-contacts .contact').click(function(){
+    var contact = $(this).attr('src');
+    $('.container-message').addClass('hidden');
+    var selettore = '.container-message[src-msg="' + contact + '"]'
+    $(selettore).removeClass('hidden')
+  });
+
+
     /*********** SEARCH ************/
     // Quando faccio "CLICK" sul 'INPUT' di SEARCH,
     $('#js_search').on("keyup", function() {
       // e inserisco una Parola,
       var value = $(this).val().toLowerCase();
-      // viene confrontata con ogni parola dentro un H4 appartenente al div.box-contact.
+      // viene confrontata con ogni parol a dentro un H4 appartenente al div.box-contact.
       $('.box-contact h4').each(
         function() {
           // Se dentro il testo, fosse inclusa la parola che ho inserito,
@@ -29,24 +51,11 @@ $('.js_delete-message').click(
     /*********************************/
 
     /************ MESSAGE **************/
-    /* Risposte Automatiche */
-    var messagesReceived = [
-      'Tutto Ok, dai!!',
-      'Non sapevo nulla',
-      'grazie',
-      'non lo sapevo',
-      'Sono contento',
-      'Raccontami qualcosa.',
-      'Vieni a casa!!',
-      'Ci vediamo domani',
-      'Sicuro???'
-    ];
-
       // Quando faccio "CLICK", si appende il 'INPUT' nel area dei messaggi.
     $('#js_enter').click(
       function(){
         sendMessage();
-
+        replyMessage();
       }
     );
 
@@ -54,7 +63,8 @@ $('.js_delete-message').click(
     $('.write-message input').keydown(
       function(event){
         if(event.which=='13'){
-        sendMessage();
+          sendMessage();
+          replyMessage();
         }
        }
      );
@@ -63,14 +73,15 @@ $('.js_delete-message').click(
 
      /*FUNZIONI*/
 
-  // FUNCTION: creaNumeroRandom(): Questa Funzione crea un numero random tenendo in considerazione come range max, la larghezza del array inserito.
-     //   --> array: array da inserire.
+     // FUNCTION: creaNumeroRandom(): Questa Funzione crea un numero random tenendo in considerazione come range max, la larghezza del array inserito.
+      //   --> array: array da inserire.
      function creaNumeroRandom(array){
          var numero = Math.round(Math.random()*(array.length-1));
          return numero;
        }
 
-   //  FUNCTION: addZeroToNumber(): Questa funzione aggiunge uno zero da vanti a un numero menore di 10.
+    // FUNCTION: addZeroToNumber(): Questa funzione aggiunge uno zero da vanti a un numero menore di 10.
+      //   --> number: numero da inserire.
      function addZeroToNumber(number){
        if(number < 10) {
          return '0' + number;
@@ -80,6 +91,7 @@ $('.js_delete-message').click(
 
    //  FUNCTION: sendMessage(): Questa funzione contiene il messaggio da inviare, e l'ora del messagio. C'e anche  una risposta automatica.
      function sendMessage(){
+
        var message = $('.write-message input').val();
        var template = $('.template').find('.message-right').clone();
        // console.log(template)
@@ -99,46 +111,62 @@ $('.js_delete-message').click(
 
        /* Vissualizare ultimo messaggio */
        $('.container-message').scrollTop($('.container-message').height());
+     }
 
-       /*** Risposta Automatica dopo 1 secondi ***/
+     //  FUNCTION: replyMessage(): Questa funzione genera una risposta automatica dopo 1 sec.
+     function replyMessage(){
        setTimeout(function(){
+         /* Risposte Automatiche */
+         var messagesReceived = [
+           'Tutto Ok, dai!!',
+           'Non sapevo nulla',
+           'grazie',
+           'non lo sapevo',
+           'Sono contento',
+           'Raccontami qualcosa.',
+           'Vieni a casa!!',
+           'Ci vediamo domani',
+           'Sicuro???'
+         ];
 
          var numeroRandon = creaNumeroRandom(messagesReceived);
          var template = $('.template').find('.message-left').clone();
-         // console.log(template)
+
+        // Inserire Ora.
+         var date = new Date();
+         var currentHours = date.getHours();
+         var currentMinutes = date.getMinutes();
+         var currentTime = addZeroToNumber(currentHours) + ':' + addZeroToNumber(currentMinutes);
+         template.find('.message-hour').text(currentTime);
 
          template.find('.box-left p').text(messagesReceived[numeroRandon]);
-         template.find('.message-hour').text(currentTime);
+
          $('.wrapper-message').append(template);
 
          /* Vissualizare ultimo messaggio */
          $('.container-message').scrollTop($('.container-message').height());
-
-
        }, 1000);
      }
 
+     //  FUNCTION: activeSettingsMessages(): Questa Funzione attiva le impostazioni dei messaggi, e ti da la opzione di eliminare il messagio.
+     function activeSettingsMessages(){
+       $('.icon-settings-message').click(
+         function(){
+           $(this).parents().siblings('.settings-message').toggleClass('active');
+           $(this).parents().parents().siblings().find('.settings-message').removeClass('active')
+
+           $('.js_delete-message').click(
+             function(){
+             $(this).parents('.js_msg').addClass('hidden');
+           }
+           );
+         }
+       );
+     }
 
      /*END FUNZIONI*/
 
 
 
   }
-
 );
-
-/***/ /***/ /***/ /***/ /***/
-function activeSettingsMessages(){
-  $('.icon-settings-message').click(
-    function(){
-      $(this).parents().siblings('.settings-message').toggleClass('active');
-      $(this).parents().parents().siblings().find('.settings-message').removeClass('active')
-
-    }
-  );
-}
-
-
-
-
-/***/ /***/ /***/ /***/ /***/
